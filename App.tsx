8856ec4b6e61fe54c37950ed9a605c7918eef273
distvision <furede2@gmail.com>
2022-07-19 03:42:36 +0200

    Setting SignIn component and states

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from "@expo-google-fonts/roboto";
 
-import { SignIn } from "./src/screens/Signin";
+import { THEME } from "./src/styles/theme";
+import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,47 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { FontAwesome, Octicons } from "@expo/vector-icons";
+import Logo from "../assets/logo_primary.svg";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { colors } = useTheme();
+
   return (
-    <View>
-      <Text>Ola Fred!</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse a sua conta
+      </Heading>
+
+      <Input
+        onChangeText={setName}
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon
+            as={<FontAwesome name="envelope-o" color={colors.gray[300]} />}
+            ml={4}
+          />
+        }
+      />
+      <Input
+        onChangeText={setPassword}
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={
+          <Icon as={<Octicons name="key" color={colors.gray[300]} />} ml={4} />
+        }
+        secureTextEntry
+      />
+
+      <Button title="Entrar" w="full" />
+    </VStack>
   );
 }
