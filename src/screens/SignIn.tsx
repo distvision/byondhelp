53f89f1f4a6ed5fcd4a4fbe3cc4dcf58d0f68798
distvision <furede2@gmail.com>
2022-08-11 05:11:06 +0200

    SignIn (user authenticated)

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,8 +6,10 @@ import { FontAwesome, Octicons } from "@expo/vector-icons";
 import Logo from "../assets/logo_primary.svg";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { isLoading } from "expo-font";
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -17,6 +19,27 @@ export function SignIn() {
     if (!email || !password) {
       return Alert.alert("Entrar", "Informe e-mail e senha");
     }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail invalido.");
+        }
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha invalida.");
+        }
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha invalida.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possivel acessar");
+      });
   }
 
   return (
@@ -48,7 +71,12 @@ export function SignIn() {
         secureTextEntry
       />
 
-      <Button title="Entrar" w="full" />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSingIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
