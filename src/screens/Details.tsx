45bbc037656831c435e4ff133b2931f5bd477d40
distvision <furede2@gmail.com>
2022-08-13 00:50:09 +0200

    cardDetails

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
-import { VStack, Text, HStack, useTheme } from "native-base";
-import { useRoute } from "@react-navigation/native";
+import { VStack, Text, HStack, useTheme, ScrollView, Box } from "native-base";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 import firestore from "@react-native-firebase/firestore";
 
-import { CircleWavyCheck, Hourglass } from "phosphor-react-native";
+import {
+  CircleWavyCheck,
+  Hourglass,
+  DesktopTower,
+  ClipboardText
+} from "phosphor-react-native";
 
 import { Header } from "../components/Header";
 import { Orderprops } from "../components/Order";
 import { OrderFirestoreDTO } from "../DTOs/OrderDTO";
 import { dateFormat } from "../utils/firestoreDateFormat";
 import { Loading } from "../components/Loading";
+import { CardDetails } from "../components/CardDetails";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { Alert } from "react-native";
 // imports ends here
 
 type RouteParams = {
@@ -28,9 +37,35 @@ export function Details() {
   const [isLoading, setIsLoading] = useState(true);
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
 
+  const navigation = useNavigation();
   const { colors } = useTheme();
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+
+  function handleOrderClosed() {
+    if (!solution) {
+      return Alert.alert(
+        "Solicitação",
+        "Informe a solução para encerrar a solicitação"
+      );
+    }
+    firestore()
+      .collection<OrderFirestoreDTO>("orders")
+      .doc(orderId)
+      .update({
+        status: "closed",
+        solution,
+        closed_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert("Solicitação", "Solicitação encerada");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert("Solicitação", "Não foi possivel encerrar a solicitação");
+      });
+  }
 
   useEffect(() => {
     firestore()
@@ -69,15 +104,60 @@ export function Details() {
 
   return (
     <VStack flex={1} bg="gray.700">
-      <Header title="Solicitação" />
-
+      <Box px={6} bg="gray.600">
+        <Header title="Solicitação" />
+      </Box>
       <HStack bg="gray.500" justifyContent="center" p={4}>
         {order.status === "closed" ? (
           <CircleWavyCheck size={22} color={colors.green[300]} />
         ) : (
-          <Hourglass size={22} color={colors.green[300]} />
+          <Hourglass size={22} color={colors.secondary[700]} />
         )}
+        <Text
+          fontSize="sm"
+          color={
+            order.status === "closed"
+              ? colors.green[300]
+              : colors.secondary[700]
+          }
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === "closed" ? "finalizado" : "em andamento"}
+        </Text>
       </HStack>
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails
+          title="equipamento"
+          description={`Patrimonio ${order.patrimony}`}
+          icon={DesktopTower}
+        />
+        <CardDetails
+          title="descrição do problema"
+          description={order.description}
+          icon={ClipboardText}
+          footer={`Registrado em ${order.when}`}
+        />
+        <CardDetails
+          title="solução"
+          icon={CircleWavyCheck}
+          description={order.solution}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+          {order.status === "open" && (
+            <Input
+              placeholder="Descrição da solução"
+              onChangeText={setSolution}
+              textAlignVertical="top"
+              multiline
+              h={24}
+            />
+          )}
+        </CardDetails>
+      </ScrollView>
+      {!order.closed && (
+        <Button title="Encerar solicitação" m={5} onPress={handleOrderClosed} />
+      )}
     </VStack>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -30,9 +30,11 @@ export function SignIn() {
         if (error.code === "auth/invalid-email") {
           return Alert.alert("Entrar", "E-mail invalido.");
         }
+
         if (error.code === "auth/wrong-password") {
           return Alert.alert("Entrar", "E-mail ou senha invalida.");
         }
+
         if (error.code === "auth/user-not-found") {
           return Alert.alert("Entrar", "E-mail ou senha invalida.");
         }
